f8cae37f24c64859038ac64cacbec120546b2b74
rockan007 <rockan007@gmail.com>
2016-09-27 12:04:01 +0800

    gride 全自动模式

--- js/utils/grideControl.js
+++ js/utils/grideControl.js
@@ -1,4 +1,7 @@
 /**
+ * @anthor an
+ */
+/**
  * 创建gride
  * @param {Object} array
  */
@@ -7,12 +10,13 @@ var createGirde = function(array) {
 		array.forEach(function(map, index, array) {
 			console.log("字符串：" + map.key + "图标：" + map.value)
 			var li = document.createElement('li')
+			var bgColor=getRandomColor();
 			li.className = "mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3";
 			li.innerHTML = '<a href="#">' +
-				'<img class="mui-icon" src="'+map.value+'"></img>' +
+				'<img class="mui-icon circular-square" src="' + map.value + '"style="background-color:'+bgColor+';"></img>' +
 				'<div class="mui-media-body">' + map.key + '</div>' +
 				'</a>';
-			gride.insertBefore(li, gride.firstChild)
+			gride.appendChild(li)
 		})
 	}
 	/**
@@ -20,20 +24,27 @@ var createGirde = function(array) {
 	 * @param {Object} array
 	 */
 var putGrideData = function(array) {
-		createGirde(array);
-	}
-	/**
-	 * 数据 数据
-	 */
-var createArray = function() {
-	var value = {
-		key: "你好",
-		value:"http://d26uhratvi024l.cloudfront.net/gsc/QL2YT7/c6/06/42/c60642dfee1c4b2bbbfa830327c6eeb7/images/公文主页/u38.png?token=00b0dee150e8bfe6d6cbc0b6bf28ebae"
-	}
-	var array = new Array();
-	for(i = 0; i < 9; i++) {
-		array.push(value);
+	createGirde(array);
+}
+
+/**
+ * 数据 数据
+ */
+var createArray = function(chars, imgUrls) {
+	var array=new Array();
+	for(i = 0; i < chars.length; i++) {
+		var value = {
+			key: chars[i],
+			value: imgUrls[i]
+		}
+		array.push(value)
 	}
 	console.log(JSON.stringify(array))
 	return array;
+}
+/**
+ * 创建随机颜色
+ */
+var getRandomColor = function(){
+  return '#'+('00000'+(Math.random()*0x1000000<<0).toString(16)).slice(-6);
 }
