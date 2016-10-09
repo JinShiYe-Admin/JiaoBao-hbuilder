79262d3e59307962e96174953799f108aa0a31ad
rockan007 <rockan007@gmail.com>
2016-10-09 11:34:07 +0800

    字体适配

--- js/utils/grideControl.js
+++ js/utils/grideControl.js
@@ -28,7 +28,7 @@ var createGirde = function(gride,array) {
 			//子控件的innerHTML
 			li.innerHTML = '<a href="#">' +
 				'<img class="mui-icon circular-square" src="' + map.value + '" style="background-color:'+bgColor+';"></img>' +
-				'<div class="mui-media-body" style="font-size:12px;">' + map.key + '</div>' +
+				'<h6 class="mui-media-body">' + map.key + '</h6>' +
 				'</a>';
 			/**
 			 * 子控件加载点击监听事件
