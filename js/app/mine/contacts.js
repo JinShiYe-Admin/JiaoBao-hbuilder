595c8fc50a6d91c79dcaacf59c33be23f3e8c9da
rockan007 <rockan007@gmail.com>
2016-10-13 10:33:07 +0800

    写入数据

---
+++ js/app/mine/contacts.js
@@ -0,0 +1,5 @@
+var contacts=(function($){
+	var names=['艾洁','冰冰叔','代勇','党飞','昞哥','党振东','不留名','赵家人','李家兴','孙由爷'];
+	var imgs=['../../image/mine/img0.jpeg','../../image/mine/img1.jpeg','../../image/mine/img2.jpg',
+	'../../image/mine/img3.jpg','../../image/mine/img4.jpg','../../image/mine/img5.jpg','../../image/mine/img6.jpg']
+})(mui);
