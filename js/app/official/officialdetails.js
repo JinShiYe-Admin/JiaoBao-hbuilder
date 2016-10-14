655a913ffeb3cc06a5e8689be16aa490579c8a4b
BugHunter1234 <444811716@qq.com>
2016-10-14 15:43:48 +0800

    公文详情调整界面和增加注释

--- js/app/official/officialdetails.js
+++ js/app/official/officialdetails.js
@@ -1,5 +1,7 @@
 /**
- * 公告详情
+ * 作者：莫尚霖
+ * 时间：2016-09-27
+ * 描述：公文详情
  */
 var officialdetails = (function($) {
 
@@ -10,10 +12,14 @@ var officialdetails = (function($) {
 	 */
 	var addTop = function(mElement, topData) {
 		var html1 = '<div class="mui-row"><div class=" mui-col-xs-2 mui-col-sm-2">';
+		//头像
 		var html2 = '<div align=center><img class="offical-bottom-image" src="'+topData[0]+'"></div>';
+		//姓名
 		var html3 = '<div class="offical-name">'+topData[1]+'</div></div>';
 		var html4 = '<div class="mui-col-xs-10 mui-col-sm-10">';
+		//内容
 		var html5 = '<font class="offical-content">'+topData[2]+'</font><br />';
+		//时间
 		var html6 = '<font class="offical-time">'+topData[3]+'</font></div></div>';
 
 		var html = html1 + html2 + html3 + html4 + html5 + html6;
@@ -41,9 +47,12 @@ var officialdetails = (function($) {
 		var htmlReply = '';
 
 		var html1 = '<div class="mui-row"><div class="mui-col-xs-12 mui-col-sm-12">';
+		//标题
 		var html2 = '<div class="offical-bottom-title">' + bottomData[0] + '</div></div>';
 		var html3 = '<div class="mui-col-xs-2 mui-col-sm-2">';
+		//部门头像
 		var html4 = '<div align=center><img class="offical-bottom-image" src="' + bottomData[1] + '"></div>';
+		//部门名
 		var html5 = '<div class="offical-bottom-department">' + bottomData[2] + '</div></div>';
 		var html6 = '<div class="mui-col-xs-10 mui-col-sm-10"><div class="offical-reply"><div class="offical-arrow"></div><div class=" offical-arrow offical-arrow2"></div>';
 
@@ -55,9 +64,12 @@ var officialdetails = (function($) {
 				html7 = '<div class="offical-bottom-reply-content-line"></div>';
 			}
 			var html8 = '<div><font><p class="mui-icon mui-icon-contact"></p>';
+			//批复人姓名
 			var html9 = '<font class="offical-bottom-reply-name">' + element[0] + '</font>';
+			//时间
 			var html10 = '<font class="offical-bottom-reply-name">' + element[1] + '</font>';
 			var html11 = '</font><br /><div class="offical-bottom-reply-content">';
+			//内容
 			var html12 = '<font>' + element[2] + '</font></div></div>';
 			htmlReply = htmlReply + html7 + html8 + html9 + html10 + html11 + html12;
 		});
